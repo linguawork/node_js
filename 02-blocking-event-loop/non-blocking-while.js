7454e0cf24ee10a4a01eef25d44bc6b937909d8f
linguawork <mixedworldcitizen@gmail.com>
2025-01-04 16:46:45 +0100

    Разблокировка вечного цикла в функции-этапе цикла событий

--- 02-blocking-event-loop/non-blocking-while.js
+++ 02-blocking-event-loop/non-blocking-while.js
@@ -1,24 +1,23 @@
 
  /*
- Практика цикла while без блокирования
- 3:34:28
- 
+   Практика цикла while без блокирования
+   3:34:28
+   
+   используем setImmediate
+   Она выполняется раз в итерацию (это важно!!!)
+   или в эту итерацию, или в следующую
 
- используем setImmediate
- Она выполняется раз в итерацию
-
- или в эту итерацию, или в следующую
+   setImmediate благодаря своей способности 
+   исполнятся раз в итерацию может разрывать бесконечное 
+   выполнение цикла на каком-то этапе в event loop
  */
 
-const { rejects } = require("assert")
+const fs = require('fs')
+let isRunning = true
 
- let isRunning = true
-
-
-
- setTimeout(
-    ()=> isRunning = false,10
- )
+setTimeout(
+    ()=> isRunning = false,0
+)
 
 process.nextTick(
     ()=>console.log('Next tick')
@@ -28,7 +27,10 @@ process.nextTick(
 function setImmediatePromise(){
    return new Promise(
       (resolve, reject) =>{
-         setImmediate(
+         // выполняется хотя бы один раз в итерации
+         // поэтому она прерывает постоянный цикл
+         setImmediate( 
+            // if we comment setImmediate, то resolve уйдет в вечный цикл
             ()=>resolve()
          )
          // resolve()
@@ -39,7 +41,6 @@ function setImmediatePromise(){
 
 
 //async await это синтаксич надстройка над промисами
-
 async function whileLoop(){
    while(isRunning){
       console.log('While is running...')
