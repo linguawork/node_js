8155cf68d0c49a956ddb098b3582bb058cf8495f
linguawork <mixedworldcitizen@gmail.com>
2025-01-15 09:30:29 +0100

    1:25:52.Создание своего фреймворка по типу express

--- UlbiNode/index.js
+++ UlbiNode/index.js
@@ -6,7 +6,16 @@
 
 const http = require('http');
 
+
+//создаем событие чтобы работать с функ handler
+const EventEmitter =require('events')
+
 const PORT = process.env.PORT || 5000;
+
+//нужны будут события для handler
+const emitter = new EventEmitter();
+
+
 
 
 class Router{
@@ -38,12 +47,53 @@ class Router{
             (like 'GET', 'POST', or 'DELETE').
         */
         if(!this.endpoints[path]){
+            //if no path like "/users", we initialize with empty obj
             this.endpoints[path] = {}
         }
+        // '/users' 
+        // сохраняем в другую переменную
+        const endpoint = this.endpoints[path]
 
+        // обращаемся к методу внутри path [GET, POST, PUT] [GET, POST, PUT, DELETE]
+        // проверяем наличие метода, если он уже есть
+        if(endpoint[method]){
+            throw new Error(`[${method}] по адресу ${path} уже существует`  )
+        }
+        //если метода нет, то цепляем handler и событие к нему
+        // import event
+        endpoint[method] = handler
+        /*
+            1:24:49 
+            Событие создаем по особому названию -
+            "[path]:[method]' :
+
+                [/users]:[get]
+                [/users]:[post]
+                [/comments]:[delete]
+
+            Можно писать по-разному название события, 
+            это один из вариантов
+        */
+        emitter.on(`[${path}]:[${method}]`, (req, res) => {
+            //внутри события используем handler с двумя стримами
+            //принимаем от пользователя запрос и отправляем ответ
+            handler(req, res)
+            //1:25:33
+            //теперь можно использовать класс Router
+
+        })
     }
-
 }
+
+
+/* скобки () — для передачи аргументов в конструктор
+Когда создается экземпляр класса в JavaScript, 
+используется синтаксис с new и скобками ()
+ для вызова конструктора класса. 
+*/
+const router = new Router()
+
+
 
 
 const server = http.createServer((req, res) => {
