58d94aea34356a00c2a5f82e4f0e9da7f24736e8
linguawork <mixedworldcitizen@gmail.com>
2025-01-15 18:21:44 +0100

    1:34, In Application class added method listen and imported inside index.js Application and Router class and re-written the logic. All works fine.

--- UlbiNode/index.js
+++ UlbiNode/index.js
@@ -4,22 +4,8 @@
     создание своего фреймворка по типу express.js
 */
 
-const http = require('http');
-
-
-//создаем событие чтобы работать с функ handler
-const EventEmitter =require('events')
 
 const PORT = process.env.PORT || 5000;
-
-//нужны будут события для handler
-const emitter = new EventEmitter();
-
-//импорт класса
-const Router = require('./framework/Router')
-
-
- 
 
 
 /* 
@@ -28,26 +14,33 @@ const Router = require('./framework/Router')
     используется синтаксис с new и скобками ()
     для вызова конструктора класса. 
 */
-const router = new Router()
+//импорт класса
+const Router = require('./framework/Router')
 //router.request('GET')
-
 /* 
     Можно вызывать так, используя каждый метод
     router.request('GET')
-
+    
     А можно в классе router написать 
     4 метода, используя request внутри них
-
+    
     get(path, handler){
-    this.request('GET', path, handler)
-    }
-
+        this.request('GET', path, handler)
+        }
+        
     и тд
 */
 
+//import of class Application 
+const App = require('./framework/Application')
 
+const app = new App()//dont forget to instantiate after import
+
+const router = new Router() //dont forget to instantiate after import
+       
 //логика ответа
 router.get('/users', (req, res)=>{
+    console.log("Here")
     res.end('YOU HAVE SENT THE REQUEST TO /USERS')
 })
 
@@ -55,47 +48,8 @@ router.get('/posts', (req, res)=>{
     res.end('YOU HAVE SENT THE REQUEST TO /POSTS')
 })
 
-
-
-
-const server = http.createServer((req, res) => {
-    
-    //эмитим события, которые прописали в логике по названию 
-    //события, то есть по шаблону события: 
-    //`[${path}]:[${method}]`
-    //параметры такие же как в функции emitter.on(`[${path}]:[${method}]`, (req, res)
-   const emitted = emitter.emit(`[${req.url}]:[${req.method}]`, req, res)
-    
-
-   //если неизвестный url, то закрываем поток, 
-   //чтобы он не висел
-   //endpoints work normally
-   if(!emitted){
-        res.end()
-    }
-    /*
-    The line below cause crashing:
-    In your framework, you're emitting an event 
-    like [path]:[method] 
-    which triggers the corresponding handler. 
-    If the handler itself calls res.end() or 
-    sends a response, that means the connection 
-    has already been closed. Trying to call 
-    res.end(req.url) afterward would result in
-    a crash or undefined behavior.
-
-    Your current code does not need to call 
-    res.end(req.url) at all in the server creation. 
-    The router.get('/users', handler) already sends 
-    a response back when a request is received 
-    at /users.
-    
-    */
-    
-    
-    
-    // res.end(req.url); 
-});
+//закинули router c функциями get в app
+app.addRouter(router)
 
 
 /*
@@ -103,9 +57,15 @@ const server = http.createServer((req, res) => {
     первый арг - Порт
     второй - коллбек
 */
-server.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-});
+        // server.listen(PORT, () => {
+        //     console.log(`Server started on port ${PORT}`);
+        // });
+
+//функция нужна чтобы запускать http server
+app.listen(PORT, 
+    () => console.log(`Server started on port ${PORT}`)
+)
+
 //run node index.js in terminal or npm start with nodemon
 
 
