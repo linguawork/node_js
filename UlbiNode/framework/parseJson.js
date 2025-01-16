6ea49ce130db421b1b1bd55ec1daf525308a015c
linguawork <mixedworldcitizen@gmail.com>
2025-01-16 15:28:33 +0100

    comments added

--- UlbiNode/framework/parseJson.js
+++ UlbiNode/framework/parseJson.js
@@ -1,6 +1,25 @@
 //middleware, по сути это функция
 
+
+
 module.exports = (req, res) =>{
+/*
+Although your method of overriding res.send is possible, 
+it's more flexible and safer to use a middleware that 
+sets the correct headers and handles JSON stringification 
+without disrupting the default functionality of res.send().
+ This approach offers more maintainability and preserves the
+ intended behavior of your framework or server.
+
+
+Короче, хотя send() по умолчанию сам назначает
+Content-type и JSON.stringify, 
+лучше писать по -старинке чтобы не было проблем
+*/
+
+
+
+
     res.send = (data) =>{
         res.writeHead(200, {
             'Content-type': 'application/json'
