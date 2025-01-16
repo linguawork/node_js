c01826d82b72b68fa87c46bc5c6d440eb79a9888
linguawork <mixedworldcitizen@gmail.com>
2025-01-16 10:06:16 +0100

    Installed and researched PostMan on my pc, got requests withinlocal network. Postman does not work on school comps

---
+++ UlbiNode/framework/postman.js
@@ -0,0 +1,74 @@
+/*
+    Information about post-man
+*/
+
+
+/*
+    What do you know about postman?
+
+
+    Postman is a popular collaboration platform for API development, 
+    which provides tools for building, testing, and managing APIs. 
+    It simplifies working with APIs by offering a user-friendly interface 
+    to send requests, inspect responses, and automate testing.
+
+    Here are some key features of Postman:
+
+    API Testing: Postman allows developers to send HTTP requests 
+    (GET, POST, PUT, DELETE, etc.) to APIs and view the responses. 
+    This makes it easier to test endpoints and verify that APIs 
+    function correctly.
+
+    Collection: Postman supports organizing requests into collections, 
+    which are groups of related API calls. 
+    Collections can be shared among team members, 
+    allowing for easier collaboration.
+
+    Environment Variables: Postman enables the use of variables for 
+    different environments 
+    (e.g., development, staging, production). This helps manage 
+    API testing across different stages of development.
+
+    Automated Testing: Postman can execute tests for API responses 
+    using built-in JavaScript-based test scripts. 
+    These tests can validate the returned status codes, data format, 
+    or specific values in the response body.
+
+    Mock Servers: Postman allows users to simulate an API server using mock servers. 
+    This can be particularly useful for front-end developers who need to test their 
+    applications before the actual API is available.
+
+    API Documentation: You can automatically generate and publish 
+    documentation for your APIs using Postman. 
+    This helps share API specifications with your team or external developers.
+
+    Collaboration: Teams can collaborate on API development in real-time, 
+    share collections, 
+    and keep track of changes made to the requests and environments.
+
+    Monitor: Postman provides a monitoring feature that enables users 
+    to schedule tests and monitor API performance and uptime.
+
+    Postman is widely used by software developers, QA engineers, 
+    and other professionals involved in the API lifecycle.
+
+*/
+
+/*
+Пробовал подключиться внутри VM но возник конфликт в портах, поэтому 
+перешел на другой комп. Ну и на школькном компе не запускается 
+Postman
+
+Можно подключаться к серверу, который на школьном компе с другого компа
+по локальной сети с указанием порта, если находишься в той же сети
+
+Также можно подключиться к серверу с другой сети, не локальной.
+Нужно настроить ssh connection и дальше работать как с локальной сетью
+
+Я запустил Postman у себя на ноутбуке и нормально,
+запрос приходит и в браузере и в Postman
+
+
+
+
+*/
--- UlbiNode/index.js
+++ UlbiNode/index.js
@@ -5,7 +5,7 @@
 */
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 //import of class Application 
 const App = require('./framework/Application')
 const userRouter = require('./src/user-router')
