adcf1bfee4b48671fec32e3eda8832de67d1e68d
linguawork <mixedworldcitizen@gmail.com>
2025-01-17 12:04:34 +0100

    Started Intro to MongoDB, need to register

---
+++ UlbiNode/mongoDB/comments.js
@@ -0,0 +1,23 @@
+/**
+     to work with mongoDB  install the node package:
+    npm i mongoose
+ */
+
+ /*
+    In summary:
+    MongoDB is the open-source database software 
+    that can be self-hosted.
+
+    MongoDB Atlas
+    is a fully managed cloud version of MongoDB that abstracts away 
+    the operational tasks associated with database management, 
+    making it easier to use and scale.
+    If you're looking for a hassle-free, cloud-based solution,
+    MongoDB Atlas is a great option. If you need complete 
+    control over your database environment and prefer managing 
+    it yourself, then self-hosting MongoDB could be more suitable.
+ 
+ */
+
+
+//https://www.mongodb.com/cloud/atlas/register
